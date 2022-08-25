a50a56abfd64c8ca110cae1c05a7a22a643b9969
Leslie <32489029+LeslieeeeHwang@users.noreply.github.com>
2022-08-25 19:43:06 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,16 +5,16 @@ module.exports = {
     siteUrl: 'https://yihong.run',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
-    // navLinks: [
-    //   {
-    //     name: 'Blog',
-    //     url: 'https://yihong.run/running',
-    //   },
-    //   {
-    //     name: 'About',
-    //     url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
-    //   },
-    // ],
+    navLinks: [
+      {
+        name: 'Blog',
+        url: 'https://yihong.run/running',
+      },
+      {
+        name: 'About',
+        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      },
+    ],
   },
   plugins: [
     'gatsby-transformer-json',
